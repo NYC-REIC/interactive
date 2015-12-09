c92e30b159cbd654e2dad64ba39700746da78806
Chris Henrick <chrishenrick@gmail.com>
2015-12-09 17:54:34 -0500

    d3 histogram working

--- js/app.graph.js
+++ js/app.graph.js
@@ -16,9 +16,9 @@ var app = (function(parent, d3){
 
 
       var binmargin = 0.2,
-          margin = {top: 10, right: 30, bottom: 30, left: 30},
-          width = 500 - margin.left - margin.right,
-          height = 150 - margin.top - margin.bottom;
+          margin = {top: 10, right: 30, bottom: 30, left: 60},
+          width = 425 - margin.left - margin.right,
+          height = 200 - margin.top - margin.bottom;
 
       var xmin = minbin - 1,
           xmax = maxbin + 1;
@@ -46,14 +46,80 @@ var app = (function(parent, d3){
 
       console.log(histdata);
 
-      // var x = d3.scale.linear()
-      //     .domain([minbin, maxbin])
-      //     .range([0, width]);
+      var x = d3.scale.linear()
+          .domain([0, (xmax - xmin)])
+          .range([0, width]);
 
-      // var data = d3.layout.histogram()
-      //     .bins(x.ticks(numbins))
-      //     (values);
+      var x2 = d3.scale.linear()
+          .domain([xmin, xmax])
+          .range([0, width]);
 
+      var y = d3.scale.linear()
+          .domain([0, d3.max(histdata, function(d){
+                  return d.numFlips;
+                })])
+          .range([height, 0]);
+
+      var xAxis = d3.svg.axis()
+          .scale(x2)
+          .tickFormat(function(d) {
+            return d.toString();
+          })
+          .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .ticks(8)
+          .orient("left")
+
+      var svg = d3.select(".graph.ui").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var bar = svg.selectAll(".bar")
+          .data(histdata)
+          .enter()
+          .append("g")
+          .attr("class", "bar")
+          .attr("transform", function(d,i) {
+              return "translate(" + x2(i * binsize + minbin) + "," + y(d.numFlips) + ")"; 
+            });
+      
+      bar.append("rect")
+        .attr("x", x(binmargin))
+        .attr("width", x(binsize - 2 * binmargin))
+        .attr("height", function(d) { return height - y(d.numFlips); });
+
+      // x-axis & labels
+      svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+      // no need to add the label "year"
+      // svg.append("text")
+      //   .attr("class", "xlabel")
+      //   .attr("text-anchor", "middle")
+      //   .attr("x", width / 2)
+      //   .attr("y", height + margin.bottom)
+      //   .text("");
+
+      // y-axis & labels
+      svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(0,0)")
+        .call(yAxis);
+
+      svg.append("text")
+        .attr("class", "ylabel")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height/2))
+        .attr("dy", "1em")
+        .attr("transform", "rotate(-90)")
+        .style("text-anchor", "middle")
+        .text("Number of Flips");
     }
 
   };
